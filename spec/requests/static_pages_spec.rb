e24a4deb454b967be772c9d158bf646d53ce62f6
Muzikayise Flynn Buthelezi <muzkayise@gmail.com>
2012-10-28 00:12:07 +0200

    adding base_title to rspec file

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -2,6 +2,8 @@ require 'spec_helper'
 
 describe "Static Pages" do
 
+  let(:base_title) { "Ruby on Rails Tutorial Sample App" }
+
   describe "Home page" do
 
     it "should have the content 'Sample App'" do
@@ -11,8 +13,7 @@ describe "Static Pages" do
 
     it "should have the right title" do
       visit '/static_pages/home'
-      page.should have_selector('title', 
-        :text => "Ruby on Rails Tutorial Sample App | Home")
+      page.should have_selector('title', :text => "#{base_title} | Home")
     end
 
   end
@@ -26,8 +27,7 @@ describe "Static Pages" do
 
     it "should have the right title" do
       visit '/static_pages/help'
-      page.should have_selector('title', 
-        :text => "Ruby on Rails Tutorial Sample App | Help")
+      page.should have_selector('title', :text => "#{base_title} | Help")
     end
 
   end
@@ -41,8 +41,7 @@ describe "Static Pages" do
 
     it "should have the right title" do
       visit '/static_pages/about'
-      page.should have_selector('title', 
-        :text => "Ruby on Rails Tutorial Sample App | About Us")
+      page.should have_selector('title', :text => "#{base_title} | About Us")
     end
 
   end
@@ -56,8 +55,7 @@ describe "Static Pages" do
 
     it "should have the right title" do
       visit '/static_pages/contact'
-      page.should have_selector('title', 
-        :text => "Ruby on Rails Tutorial Sample App | Contact Us")
+      page.should have_selector('title', :text => "#{base_title} | Contact Us")
     end
 
   end
