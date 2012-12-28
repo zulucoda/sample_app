a8fb4b84465cc723ac391a5d860834f1f9904397
Muzikayise Flynn Buthelezi <muzkayise@gmail.com>
2012-12-28 16:36:17 +0200

    chapter 6 exercise

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -102,6 +102,17 @@ describe User do
     
   end
 
+  describe "email address with mixed case" do
+    let(:mixed_case_email) { "Foo@ExamPLE.CoM" }
+
+    it "should be saved as all lower-case" do
+      @user.email = mixed_case_email
+      @user.save
+      @user.reload.email.should == mixed_case_email.downcase
+    end
+    
+  end
+
   describe "when password is not present" do
 
     before { @user.password = @user.password_confirmation = " " }
