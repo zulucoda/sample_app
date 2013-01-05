a26fced23baee839fa3918631c4c19f25a742c6f
Muzikayise Flynn Buthelezi <muzkayise@gmail.com>
2013-01-05 22:36:16 +0200

    Add user microposts

--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -125,7 +125,6 @@ describe "Authentication" do
         
       end
 
-
       describe "in the Users controller" do
 
         describe "visiting the user index" do
@@ -135,6 +134,20 @@ describe "Authentication" do
         
       end
       
+      describe "in the Microposts controller" do
+
+        describe "submitting to the create action" do
+          before { post microposts_path }
+          specify { response.should redirect_to(signin_path) }
+        end
+
+        describe "submitting to the destroy action" do
+          before { delete micropost_path(FactoryGirl.create(:micropost)) }
+          specify { response.should redirect_to(signin_path) }
+        end
+        
+      end
+
     end
 
     describe "as wrong user" do
---
+++ spec/requests/micropost_pages_spec.rb
@@ -0,0 +1,46 @@
+require 'spec_helper'
+
+describe "Micropost pages" do
+  
+  subject { page }
+
+  let(:user) {  FactoryGirl.create(:user) }
+  let(:post_btn) { "Post" }
+  before { sign_in user }
+
+  describe "micropost creation" do
+    before { visit root_path }
+
+    describe "with invalid information" do
+      it "should not create a micropost" do
+        expect { click_button post_btn }.not_to change(Micropost, :count)
+      end
+
+      describe "error messages" do
+        before { click_button post_btn }
+        it { should have_content('error') }
+      end
+    end
+
+    describe "with valid information" do
+      before { fill_in 'micropost_content', with: "Lorem ipsum" }
+      it "should create a micropost" do
+        expect { click_button post_btn }.to change(Micropost, :count).by(1)
+      end
+    end
+  end
+
+  describe "micropost destruction" do
+    before { FactoryGirl.create(:micropost, user: user) }
+    
+    describe "as correct user" do
+      before { visit root_path }
+
+      it "should delete a micropost" do
+        expect { click_link "delete" }.to change(Micropost, :count).by(-1)
+      end
+
+    end
+
+  end
+end
